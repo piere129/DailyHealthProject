8117bec488cd9b3d3de3217219e031821e8a1ebb
Desseyn Pieter <Pieter@MBPvanPeter1078.lan>
2017-12-30 21:38:44 +0100

    refactored controllers, dropdown & textfields implemented + Autolayout portrait, upgraded calculator

--- DailyHealth/controllers/ViewController.swift
+++ DailyHealth/controllers/ViewController.swift
@@ -9,33 +9,87 @@
 import UIKit
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
+    
+    @IBOutlet weak var textbox1: UITextField!
+    @IBOutlet weak var dropdown1: UIPickerView!
+    
+    @IBOutlet weak var textbox2: UITextField!
+    @IBOutlet weak var dropdown2: UIPickerView!
+    
+    var foodTypes = ["fruit","vegetable","meat","drinks","other"]
+    var foods = ["1","2","3","4","5","6"]
+    var foodArray = [FoodItem]()
     
     var calculator = CaloryCalculator();
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
+        //zorgen dat dropdown boven de textfields wordt weergegeven
+        self.dropdown1.layer.zPosition = 1;
+        self.dropdown2.layer.zPosition = 1;
     }
     
-    //executed before going to next view, before segue is clicked
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let resultController = segue.destination as! ResultController
-        resultController.resultString = String(calculator.getTotal(name:nameField.text!,amount: 1))
+    //alle dropdown-gerelateerde code:
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1;
     }
-
-    @IBOutlet weak var nameField: UITextField!;
     
-    @IBAction func sendResults(_ sender: Any) {
-        if nameField.text != ""
-        {
-            performSegue(withIdentifier: "segue", sender: self)
+    //aantal rijen instellen voor dropdown-menu
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        var countrows : Int = foodTypes.count
+        if pickerView == dropdown2 {
+            countrows = foods.count
+        }
+        return countrows;
+    }
+    
+    //edit textfieldtitel met nieuwe waarde dropdown
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        self.view.endEditing(true)
+        if pickerView == dropdown1 {
+            let titleRow = foodTypes[row];
+            return titleRow;
+        }
+        else if pickerView == dropdown2 {
+            let titleRow = foods[row];
+            return titleRow;
+        }
+        return "";
+    }
+    
+    //doet dropdown verdwijnen na selectie item, en verandert 2e dropdown bij verandering eerste
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView == dropdown1 {
+            self.textbox1.text = self.foodTypes[row]
+            self.textbox2.text = "";
+            self.foodArray = calculator.getArrayOfType(type: textbox1.text!);
+            for (index, element) in foodArray.enumerated() {
+                foods[index] = element.name;
+            }
+            self.dropdown1.isHidden = true;
+        }
+        else if pickerView == dropdown2 {
+            self.textbox2.text = self.foods[row]
+            self.dropdown2.isHidden = true;
         }
     }
     
+    //dropdown2 enablen bij selectie eerste knop
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField == self.textbox1
+        {
+            self.dropdown1.isHidden = false;
+            self.textbox2.isEnabled = true;
+            self.textbox1.endEditing(true);
+        }
+        else if textField == self.textbox2
+        {
+            self.dropdown2.isHidden = false;
+            self.textbox2.endEditing(true);
+        }
+    }
     
-
 }
 
 
--- DailyHealth/models/Calculator.swift
+++ DailyHealth/models/Calculator.swift
@@ -10,6 +10,11 @@ import Foundation
 struct CaloryCalculator{
     
     var food = [FoodItem]()
+    var fruits = [FoodItem]()
+    var vegetables = [FoodItem]()
+    var meat = [FoodItem]()
+    var drinks = [FoodItem]()
+    var other = [FoodItem]()
 
     
     init(){
@@ -22,35 +27,50 @@ struct CaloryCalculator{
         let apple = FoodItem(foodType: FoodType.FRUIT,name: "apple",caloryAmount: 52.1)
         let pear = FoodItem(foodType: FoodType.FRUIT,name: "pear",caloryAmount: 57.1)
         let banana = FoodItem(foodType: FoodType.FRUIT,name: "banana",caloryAmount: 88.7)
+        let peach = FoodItem(foodType: FoodType.FRUIT,name: "peach",caloryAmount: 39.4)
+        let kiwi = FoodItem(foodType: FoodType.FRUIT,name: "kiwi",caloryAmount: 60.9)
+        let pineapple = FoodItem(foodType: FoodType.FRUIT,name: "pineapple",caloryAmount: 50)
         
-        food.append(apple); food.append(pear); food.append(banana);
+        fruits.append(apple); fruits.append(pear); fruits.append(banana);
+        fruits.append(peach); fruits.append(kiwi); fruits.append(pineapple);
+        food.append(contentsOf: fruits)
+        
         
         //per 100 gram
         let cucumber = FoodItem(foodType: FoodType.VEGETABLE,name: "cucumber",caloryAmount: 15.5)
         let tomato = FoodItem(foodType: FoodType.VEGETABLE,name: "tomato",caloryAmount: 17.7)
         let broccoli = FoodItem(foodType: FoodType.VEGETABLE,name: "broccoli",caloryAmount: 33.7)
         let potato = FoodItem(foodType: FoodType.VEGETABLE,name: "potato",caloryAmount: 76.7)
+        let lettuce = FoodItem(foodType: FoodType.VEGETABLE,name: "lettuce",caloryAmount: 23.2)
+        let peas = FoodItem(foodType: FoodType.VEGETABLE,name: "peas",caloryAmount: 81)
         
-        food.append(cucumber); food.append(tomato);
-        food.append(broccoli); food.append(potato);
+        vegetables.append(cucumber); vegetables.append(tomato); vegetables.append(broccoli);
+        vegetables.append(potato); vegetables.append(lettuce); vegetables.append(peas);
+        food.append(contentsOf: vegetables)
         
         //per 100 gram
         let chicken = FoodItem(foodType: FoodType.MEAT,name: "chicken",caloryAmount: 239)
         let sausage = FoodItem(foodType: FoodType.MEAT,name: "sausage",caloryAmount: 300.9)
         let beef = FoodItem(foodType: FoodType.MEAT,name: "beef",caloryAmount: 250.5)
         let fish = FoodItem(foodType: FoodType.MEAT,name: "fish",caloryAmount: 205.8)
+        let mincedMeat = FoodItem(foodType: FoodType.MEAT, name: "minced meat", caloryAmount:240.7)
+        let pork = FoodItem(foodType: FoodType.MEAT, name: "pork", caloryAmount:242.1)
         
-        food.append(chicken); food.append(sausage);
-        food.append(beef); food.append(fish);
+        meat.append(chicken); meat.append(sausage); meat.append(beef);
+        meat.append(fish); meat.append(mincedMeat); meat.append(pork);
+        food.append(contentsOf: meat);
         
         // per 10 cl
         let milk = FoodItem(foodType: FoodType.DRINK,name: "milk",caloryAmount: 42.3)
         let cola = FoodItem(foodType: FoodType.DRINK,name: "cola",caloryAmount: 37.5)
         let coffee = FoodItem(foodType: FoodType.DRINK,name: "coffee",caloryAmount: 0.5)
         let tea = FoodItem(foodType: FoodType.DRINK,name: "tea",caloryAmount: 1)
+        let water = FoodItem(foodType: FoodType.DRINK, name: "water", caloryAmount:0)
+        let beer = FoodItem(foodType: FoodType.DRINK, name: "beer", caloryAmount:43.3)
         
-        food.append(milk); food.append(cola);
-        food.append(coffee); food.append(tea);
+        drinks.append(milk); drinks.append(cola); drinks.append(coffee);
+        drinks.append(tea); drinks.append(water); drinks.append(beer);
+        food.append(contentsOf: drinks);
         
         //per 100 gram
         let eggs = FoodItem(foodType: FoodType.OTHER,name: "eggs",caloryAmount: 155.1)
@@ -60,8 +80,9 @@ struct CaloryCalculator{
         let pasta = FoodItem(foodType: FoodType.OTHER,name: "pasta",caloryAmount: 131)
         let chocolate = FoodItem(foodType: FoodType.OTHER,name: "chocolate",caloryAmount: 545.6)
         
-        food.append(eggs); food.append(yoghurt); food.append(cheese);
-        food.append(bread); food.append(pasta); food.append(chocolate);
+        other.append(eggs); other.append(yoghurt); other.append(cheese);
+        other.append(bread); other.append(pasta); other.append(chocolate);
+        food.append(contentsOf: other)
     }
     
     //geeft het calorie-aantal terug na het verwerken vh aantal g of cl gedronken
@@ -82,5 +103,17 @@ struct CaloryCalculator{
         }
         //default value
         return 0;
+    }
+    
+    func getArrayOfType(type: String) -> [FoodItem]
+    {
+        switch type {
+        case "fruit": return fruits;
+        case "vegetable": return vegetables;
+        case "meat": return meat;
+        case "drinks": return drinks;
+        case "other": return other;
+        default: return food;
+        }
     }
 }
