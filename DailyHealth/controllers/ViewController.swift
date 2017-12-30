6cbaeb30730852bdc1ef32506cabb0a4f7cb879e
Desseyn Pieter <Pieter@MBPvanPeter1078.lan>
2017-12-30 12:00:07 +0100

    CaloryCalculator works, navigation between views remembers state

--- DailyHealth/controllers/ViewController.swift
+++ DailyHealth/controllers/ViewController.swift
@@ -8,17 +8,21 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
+    
+    var calculator = CaloryCalculator();
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
     
     //executed before going to next view, before segue is clicked
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        var resultController = segue.destination as! ResultController
-        resultController.resultString = nameField.text!
+        let resultController = segue.destination as! ResultController
+        resultController.resultString = String(calculator.getTotal(name:nameField.text!,amount: 1))
     }
 
     @IBOutlet weak var nameField: UITextField!;
--- DailyHealth/models/Calculator.swift
+++ DailyHealth/models/Calculator.swift
@@ -9,11 +9,8 @@
 import Foundation
 struct CaloryCalculator{
     
-    var fruits = [FoodItem]()
-    var vegetables = [FoodItem]()
-    var meats = [FoodItem]()
-    var drinks = [FoodItem]()
-    var other = [FoodItem]()
+    var food = [FoodItem]()
+
     
     init(){
         initialiseArrays()
@@ -22,49 +19,68 @@ struct CaloryCalculator{
     mutating func initialiseArrays()
     {
         // per 100 gram
-        let apple = FoodItem(foodType: FoodType.FRUIT,name: "Apple",caloryAmount: 52.1)
-        let pear = FoodItem(foodType: FoodType.FRUIT,name: "Pear",caloryAmount: 57.1)
-        let banana = FoodItem(foodType: FoodType.FRUIT,name: "Banana",caloryAmount: 88.7)
+        let apple = FoodItem(foodType: FoodType.FRUIT,name: "apple",caloryAmount: 52.1)
+        let pear = FoodItem(foodType: FoodType.FRUIT,name: "pear",caloryAmount: 57.1)
+        let banana = FoodItem(foodType: FoodType.FRUIT,name: "banana",caloryAmount: 88.7)
         
-        fruits.append(apple); fruits.append(pear); fruits.append(banana);
-        
-        //per 100 gram
-        let cucumber = FoodItem(foodType: FoodType.VEGETABLE,name: "Cucumber",caloryAmount: 15.5)
-        let tomato = FoodItem(foodType: FoodType.VEGETABLE,name: "Tomato",caloryAmount: 17.7)
-        let broccoli = FoodItem(foodType: FoodType.VEGETABLE,name: "Broccoli",caloryAmount: 33.7)
-        let potato = FoodItem(foodType: FoodType.VEGETABLE,name: "Potato",caloryAmount: 76.7)
-        
-        vegetables.append(cucumber); vegetables.append(tomato);
-        vegetables.append(broccoli); vegetables.append(potato);
+        food.append(apple); food.append(pear); food.append(banana);
         
         //per 100 gram
-        let chicken = FoodItem(foodType: FoodType.MEAT,name: "Chicken",caloryAmount: 239)
-        let sausage = FoodItem(foodType: FoodType.MEAT,name: "Sausage",caloryAmount: 300.9)
-        let beef = FoodItem(foodType: FoodType.MEAT,name: "Beef",caloryAmount: 250.5)
-        let fish = FoodItem(foodType: FoodType.MEAT,name: "Fish",caloryAmount: 205.8)
+        let cucumber = FoodItem(foodType: FoodType.VEGETABLE,name: "cucumber",caloryAmount: 15.5)
+        let tomato = FoodItem(foodType: FoodType.VEGETABLE,name: "tomato",caloryAmount: 17.7)
+        let broccoli = FoodItem(foodType: FoodType.VEGETABLE,name: "broccoli",caloryAmount: 33.7)
+        let potato = FoodItem(foodType: FoodType.VEGETABLE,name: "potato",caloryAmount: 76.7)
         
-        meats.append(chicken); meats.append(sausage);
-        meats.append(beef); meats.append(fish);
+        food.append(cucumber); food.append(tomato);
+        food.append(broccoli); food.append(potato);
+        
+        //per 100 gram
+        let chicken = FoodItem(foodType: FoodType.MEAT,name: "chicken",caloryAmount: 239)
+        let sausage = FoodItem(foodType: FoodType.MEAT,name: "sausage",caloryAmount: 300.9)
+        let beef = FoodItem(foodType: FoodType.MEAT,name: "beef",caloryAmount: 250.5)
+        let fish = FoodItem(foodType: FoodType.MEAT,name: "fish",caloryAmount: 205.8)
+        
+        food.append(chicken); food.append(sausage);
+        food.append(beef); food.append(fish);
         
         // per 10 cl
-        let milk = FoodItem(foodType: FoodType.DRINK,name: "Milk",caloryAmount: 42.3)
-        let cola = FoodItem(foodType: FoodType.DRINK,name: "Cola",caloryAmount: 37.5)
-        let coffee = FoodItem(foodType: FoodType.DRINK,name: "Coffee",caloryAmount: 0.5)
-        let tea = FoodItem(foodType: FoodType.DRINK,name: "Tea",caloryAmount: 1)
+        let milk = FoodItem(foodType: FoodType.DRINK,name: "milk",caloryAmount: 42.3)
+        let cola = FoodItem(foodType: FoodType.DRINK,name: "cola",caloryAmount: 37.5)
+        let coffee = FoodItem(foodType: FoodType.DRINK,name: "coffee",caloryAmount: 0.5)
+        let tea = FoodItem(foodType: FoodType.DRINK,name: "tea",caloryAmount: 1)
         
-        drinks.append(milk); drinks.append(cola);
-        drinks.append(coffee); drinks.append(tea);
+        food.append(milk); food.append(cola);
+        food.append(coffee); food.append(tea);
         
         //per 100 gram
-        let eggs = FoodItem(foodType: FoodType.OTHER,name: "Eggs",caloryAmount: 155.1)
-        let yoghurt = FoodItem(foodType: FoodType.OTHER,name: "Yoghurt",caloryAmount: 58.8)
-        let cheese = FoodItem(foodType: FoodType.OTHER,name: "Cheese",caloryAmount: 402.5)
-        let bread = FoodItem(foodType: FoodType.OTHER,name: "Bread",caloryAmount: 264.6)
-        let pasta = FoodItem(foodType: FoodType.OTHER,name: "Pasta",caloryAmount: 131)
-        let chocolate = FoodItem(foodType: FoodType.OTHER,name: "Chocolate",caloryAmount: 545.6)
+        let eggs = FoodItem(foodType: FoodType.OTHER,name: "eggs",caloryAmount: 155.1)
+        let yoghurt = FoodItem(foodType: FoodType.OTHER,name: "yoghurt",caloryAmount: 58.8)
+        let cheese = FoodItem(foodType: FoodType.OTHER,name: "cheese",caloryAmount: 402.5)
+        let bread = FoodItem(foodType: FoodType.OTHER,name: "bread",caloryAmount: 264.6)
+        let pasta = FoodItem(foodType: FoodType.OTHER,name: "pasta",caloryAmount: 131)
+        let chocolate = FoodItem(foodType: FoodType.OTHER,name: "chocolate",caloryAmount: 545.6)
         
-        other.append(eggs); other.append(yoghurt); other.append(cheese);
-        other.append(bread); other.append(pasta); other.append(chocolate);
-        
+        food.append(eggs); food.append(yoghurt); food.append(cheese);
+        food.append(bread); food.append(pasta); food.append(chocolate);
+    }
+    
+    //geeft het calorie-aantal terug na het verwerken vh aantal g of cl gedronken
+    func getTotal(name: String, amount: Double) -> Double
+    {
+        //loopt door alle fooditems voor matchende naam
+        for fooditem in food
+        {
+            if fooditem.name == name {
+                if fooditem.type == FoodType.DRINK
+                {
+                    //UI vraagt voor amount in cl, dus delen door 10 hier voor correcte calorieen
+                    return amount * 0.1 * fooditem.caloryAmount
+                }
+                //UI vraagt voor amount in g, dus delen door 100 voor correcte calorieen
+                return amount * 0.01 * fooditem.caloryAmount
+            }
+        }
+        //default value
+        return 0;
     }
 }
